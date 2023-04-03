b636a8a16d656bc217e775934b38810ae3667880
Prajwal Chitode <99211609+prajwalchitode@users.noreply.github.com>
2023-04-03 22:17:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://127.0.0.1:5173",
+    origin:'*',
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
