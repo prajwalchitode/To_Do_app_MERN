24c8bb25155fa602e6fab3df26e5a575744927c2
Prajwal Chitode <99211609+prajwalchitode@users.noreply.github.com>
2023-04-07 21:58:18 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -4,12 +4,12 @@ import mongoose from "mongoose";
 // Db connnection
 export const dbconnection = ()=> {
     mongoose.
-    connect("mongodb://localhost:27017",{
+    connect("mongodb://fileuplode:fileuplode@ac-25s0y3t-shard-00-00.t8qfp9v.mongodb.net:27017,ac-25s0y3t-shard-00-01.t8qfp9v.mongodb.net:27017,
+ac-25s0y3t-shard-00-02.t8qfp9v.mongodb.net:27017/?ssl=true&replicaSet=atlas-u7w4pr-shard-0&authSource=admin&retryWrites=true&w=majority",{
     dbName:"LearningBackendApi",
 })
 .then(()=>console.log('Database connected successfully'))
 .catch(e=> console.log(e))
 };
 
-// "mongodb://fileuplode:fileuplode@ac-25s0y3t-shard-00-00.t8qfp9v.mongodb.net:27017,ac-25s0y3t-shard-00-01.t8qfp9v.mongodb.net:27017,
-// ac-25s0y3t-shard-00-02.t8qfp9v.mongodb.net:27017/?ssl=true&replicaSet=atlas-u7w4pr-shard-0&authSource=admin&retryWrites=true&w=majority"
+
